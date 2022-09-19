1c6d23ac111d4bd5583cc9fd383ce8c2e61c3b45
Alexiy Rybin <0xtapnisu@gmail.com>
2022-09-19 22:41:26 +0700

    Updated font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Comfortaa"],
+				serif: ["Comfortaa"],
+				mono: ["Comfortaa"],
+				display: ["Comfortaa"],
+				body: ["Comfortaa"],
+			},
+		},
+	},
+	plugins: [],
 };
