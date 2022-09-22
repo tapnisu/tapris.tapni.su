df727d12a5c5bce4f001c986cf858f747166b0f1
Alexiy Rybin <0xtapnisu@gmail.com>
2022-09-22 22:56:56 +0700

    Added links to commands

--- components/CommandCard.tsx
+++ components/CommandCard.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 const CommandCard = ({ command, id }: any) => {
 	return (
 		<>
 			<div className="bg-neutral-900 m-2 p-8 rounded-3xl">
-				<h2 className="text-xl text-indigo-400">/{command.name}</h2>
+				<Link href={`/commands/${id}`}>
+					<h2 className="text-xl cursor-pointer text-indigo-400">
+						/{command.name}
+					</h2>
+				</Link>
 
 				{command.description}
 			</div>
