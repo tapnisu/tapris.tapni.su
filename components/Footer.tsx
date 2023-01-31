fb0f5293effd987ef8f2e96f0f0bf5373db8bc1d
Alexiy Rybin <0xtapnisu@gmail.com>
2023-01-31 18:02:32 +0700

    smol footer code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,16 @@
 const Footer = () => {
   return (
     <>
-      <footer className="bottom-0 w-full ">
-        <div className="flex flex-row flex-wrap justify-between p-8">
-          <span className="uppercase">
-            <a href="https://github.com/tapris-bot/tapris">Github</a> / {""}
-            <a href="https://discord.com/api/oauth2/authorize?client_id=869088074758520832&scope=bot+applications.commands&permissions=294208515334">
-              Invite
-            </a>{" "}
-            / {""}
-            <a href="https://discord.gg/TwtSs7NY9t">Discord Server</a>
-          </span>
-          Copyright © 2023 Alexiy Rybin
-        </div>
+      <footer className="flex flex-row flex-wrap justify-between p-8 w-full">
+        <span className="uppercase">
+          <a href="https://github.com/tapris-bot/tapris">Github</a> / {""}
+          <a href="https://discord.com/api/oauth2/authorize?client_id=869088074758520832&scope=bot+applications.commands&permissions=294208515334">
+            Invite
+          </a>{" "}
+          / {""}
+          <a href="https://discord.gg/TwtSs7NY9t">Discord Server</a>
+        </span>
+        Copyright © 2023 Alexiy Rybin
       </footer>
     </>
   );
