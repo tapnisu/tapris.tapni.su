9f5d3f2b08b46c4416646cd55d344ca9da4ed9d6
Alexiy Rybin <0xtapnisu@gmail.com>
2023-03-07 21:28:50 +0700

    Fix colors

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,18 @@
 import { Options } from "$fresh/plugins/twind.ts";
+import * as colors from "twind/colors";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    colors: {
+      blue: colors.blue,
+      black: colors.black,
+      gray: colors.gray,
+      indigo: colors.indigo,
+      green: colors.green,
+      white: colors.white,
+      yellow: colors.yellow,
+      transparent: "transparent",
+    },
+  },
 } as Options;
