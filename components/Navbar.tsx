a2766bbd3f6a84c77c2b3895d8fd5230426bcbf4
Alexiy Rybin <0xtapnisu@gmail.com>
2023-03-07 22:02:21 +0700

    Update navbar style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,24 @@
 const Navbar = () => (
-  <nav className="sticky top-0 flex justify-center w-full">
-    <div className="bg-gray-900 dark:text-white rounded-full m-2 border-2 border-gray-800">
-      <ul className="mx-2">
-        {[
-          { name: "Home", url: "/" },
-          {
-            name: "Projects",
-            url: "/projects",
-          },
-          { name: "Setup", url: "/setup" },
-        ]?.map((info: { name: string; url: string }) => (
-          <li className="inline-block m-2" key={info.name}>
-            <a
-              href={info.url}
-              className="hover:text-indigo-400 transition-colors"
-            >
-              {info.name}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+  <nav className="sticky top-0 flex flex-row flex-nowrap w-full bg-black overflow-x-auto overflow-y-hidden whitespace-nowrap">
+    <ul className="mx-2">
+      {[
+        { name: "Home", url: "/" },
+        {
+          name: "Commands",
+          url: "/commands",
+        },
+        { name: "Api", url: "https://tapris-bot.deno.dev/api/" },
+      ]?.map((info: { name: string; url: string }) => (
+        <li className="inline-block m-4" key={info.name}>
+          <a
+            href={info.url}
+            className="hover:text-indigo-400 transition-colors"
+          >
+            {info.name}
+          </a>
+        </li>
+      ))}
+    </ul>
   </nav>
 );
 
