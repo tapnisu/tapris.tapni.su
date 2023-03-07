27f47e912d7e3266a64e76066739c217a0d1eb99
Alexiy Rybin <0xtapnisu@gmail.com>
2023-03-07 21:37:21 +0700

    Add button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,17 @@
-import { JSX } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
+import { ComponentChildren } from "preact";
 
-export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
+interface Props {
+  children: ComponentChildren;
+}
+
+export function Button(props: Props) {
   return (
-    <button
-      {...props}
-      disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 border(gray-100 2) hover:bg-gray-200"
-    />
+    <div
+      disabled={!IS_BROWSER}
+      className="bg-indigo-400 hover:bg-indigo-600 text-white m-4 p-4 rounded-full transition-colors"
+    >
+      {props.children}
+    </div>
   );
 }
