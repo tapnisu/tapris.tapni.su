cb6224383b1c23d377c6b14d8fb55d450aff2532
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-01-13 09:56:55 +0700

    Fix invite

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,18 @@
 const Navbar = () => (
   <nav className="sticky top-0 flex flex-row flex-nowrap w-full bg-black overflow-x-auto overflow-y-hidden whitespace-nowrap">
     <ul className="mx-2">
-      {[
-        { name: "Home", url: "/" },
-        {
-          name: "Commands",
-          url: "/commands",
-        },
-        { name: "Api", url: "https://tapris-bot.deno.dev/api/v1" },
-      ]?.map((info: { name: string; url: string }) => (
-        <li className="inline-block m-4" key={info.name}>
-          <a
-            href={info.url}
-            className="hover:text-indigo-400 transition-colors"
-          >
-            {info.name}
-          </a>
-        </li>
-      ))}
+      {[{ name: "Home", url: "/" }]?.map(
+        (info: { name: string; url: string }) => (
+          <li className="inline-block m-4" key={info.name}>
+            <a
+              href={info.url}
+              className="hover:text-indigo-400 transition-colors"
+            >
+              {info.name}
+            </a>
+          </li>
+        )
+      )}
     </ul>
   </nav>
 );
--- routes/invite.ts
+++ routes/invite.ts
@@ -1,3 +1,6 @@
 export function handler(_req: Request): Response {
-  return Response.redirect("https://tapris-bot.deno.dev/invite", 307);
+  const INVITE_URL =
+    "https://discord.com/api/oauth2/authorize?client_id=869088074758520832&scope=bot+applications.commands&permissions=294208515334";
+
+  return Response.redirect(INVITE_URL, 307);
 }
