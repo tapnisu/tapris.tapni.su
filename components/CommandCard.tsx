4a81cf25fc5df679be310ac37c4451c5c5957c91
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-06-18 09:27:12 +0700

    Fix lint errors

--- components/CommandCard.tsx
+++ components/CommandCard.tsx
@@ -7,17 +7,15 @@ interface CommandCardProps {
 
 const CommandCard = ({ command, id }: CommandCardProps) => {
   return (
-    <>
-      <div className="bg-neutral-900 m-2 p-8 rounded-3xl">
-        <a href={`/commands/${command.name}`}>
-          <h2 className="text-xl cursor-pointer text-indigo-400">
-            /{command.name}
-          </h2>
-        </a>
+    <div className="bg-neutral-900 m-2 p-8 rounded-3xl" key={id}>
+      <a href={`/commands/${command.name}`}>
+        <h2 className="text-xl cursor-pointer text-indigo-400">
+          /{command.name}
+        </h2>
+      </a>
 
-        {command.description}
-      </div>
-    </>
+      {command.description}
+    </div>
   );
 };
 
--- components/OptionCard.tsx
+++ components/OptionCard.tsx
@@ -7,15 +7,13 @@ interface OptionCardProps {
 
 const OptionCard = ({ option, id }: OptionCardProps) => {
   return (
-    <>
-      <div className="bg-neutral-900 m-2 p-8 rounded-3xl">
-        <span className="text-xl text-indigo-400">{option.name}</span> -{" "}
-        {option.description}
-        <h6 className="text-sm text-neutral-500">
-          {option.required ? "Required" : ""}
-        </h6>
-      </div>
-    </>
+    <div className="bg-neutral-900 m-2 p-8 rounded-3xl" key={id}>
+      <span className="text-xl text-indigo-400">{option.name}</span> -{" "}
+      {option.description}
+      <h6 className="text-sm text-neutral-500">
+        {option.required ? "Required" : ""}
+      </h6>
+    </div>
   );
 };
 
